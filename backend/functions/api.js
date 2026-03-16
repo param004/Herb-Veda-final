6a4c501b23c7a3e876627a86d65b6ec226f83225
Param Pambhar <23cs051@charusat.edu.in>
2026-03-16 13:16:50 +0530

    Fix Mongoose-Serverless hang with callbackWaitsForEmptyEventLoop

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -1,4 +1,10 @@
 const serverless = require('serverless-http');
 const app = require('../server');
 
-module.exports.handler = serverless(app);
+const handler = serverless(app);
+
+module.exports.handler = async (event, context) => {
+    // Standard fix for Mongoose + Serverless (prevents function hang)
+    context.callbackWaitsForEmptyEventLoop = false;
+    return await handler(event, context);
+};
